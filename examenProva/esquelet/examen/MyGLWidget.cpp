683b35f341fdd24b0ef9ba763cd8394c97d90ddd
Aleix Sanchis <aleixsanchis@hotmail.com>
2017-06-04 19:11:20 +0200

    Ara gira guay

--- examenProva/esquelet/examen/MyGLWidget.cpp
+++ examenProva/esquelet/examen/MyGLWidget.cpp
@@ -118,8 +118,9 @@ void MyGLWidget::viewTransform ()
   if(!primeraPersona){
     glm::mat4 View = glm::lookAt(OBS,VRP,UP);  // Matriu de posició i orientació
     // View = glm::translate(glm::mat4(1.f), glm::vec3(0, -2, -2*radiEsc));
+
+    View = glm::rotate(View, angleX, glm::vec3(1, 0, 0));
     View = glm::rotate(View, -angleY, glm::vec3(0, 1, 0));
-    View = glm::rotate(View, -angleX, glm::vec3(1, 0, 0));
     glUniformMatrix4fv (viewLoc, 1, GL_FALSE, &View[0][0]);
   }
   else{
@@ -458,7 +459,8 @@ void MyGLWidget::mouseMoveEvent(QMouseEvent *e)
   {
     // Fem la rotació
     angleY += (e->x() - xClick) * M_PI / 180.0;
-    angleX += (e->y() - yClick) * M_PI / 180.0;
+    //angleX += (yClick - e->y()) * M_PI / 180.0;
+    angleX += ( e->y() - yClick) * M_PI / 180.0;
     viewTransform ();
   }
 
